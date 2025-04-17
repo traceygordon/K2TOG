abe6f4fb14ceddca200ba55d44cc460b8dd81c46
Denise Osoria <deniseosoria04@gmail.com>
2025-04-16 21:12:53 -0400

    created config folder

---
+++ server/db/config/db.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+});  
+
+module.exports = pool;
+
+// .env file:
+// Create own .env file and add the following:
+// DB_USER=postgres
+// DB_PASSWORD=postgres
+// DB_HOST=localhost
+// DB_PORT=5432
+// DB_NAME=acme_classes_db
+// PORT=5000
+// JWT_SECRET=shhh
+
